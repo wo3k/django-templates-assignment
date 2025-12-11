87ef0a71a057561a3a1f863dc8ea9a2cfe4342ea
Belal Jnena <belaljenana2@gmail.com>
2025-12-11 16:27:44 +0200

    ass1.7

--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 
-from django.shortcuts import render
+from datetime import date
 
 def home(request):
     context = {
-        "full_name": "Belal Jnena",
+        "first_name": "belal",
+        "last_name": " Jnena",
         "student_id": "120221646",
         "address": "Palestine - Gaza",
         "email": "belaljenana2@gmail.com",
         "note": "This page is for my Templates assignment",
+        "dob": date(2004, 5, 13),
     }
     return render(request, "pages/index.html", context)
