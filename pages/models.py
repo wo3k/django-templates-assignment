9a7a6543c3f0951390e27efd20a0cafd9d6459d3
Belal Jnena <belaljenana2@gmail.com>
2025-12-31 14:54:15 +0200

    URL & Models

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    full_name = models.CharField(max_length=100)
+    university_id = models.CharField(max_length=20)
+    age = models.PositiveIntegerField()
+    date_of_birth = models.DateField()
+    address = models.TextField()
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    photo = models.ImageField(upload_to='photos/')
+
+    def __str__(self):
+        return self.full_name
--- pages/views.py
+++ pages/views.py
@@ -1,15 +1,6 @@
 from django.shortcuts import render
-
-from datetime import date
+from .models import Person
 
 def home(request):
-    context = {
-        "first_name": "belal",
-        "last_name": " Jnena",
-        "student_id": "120221646",
-        "address": "Palestine - Gaza",
-        "email": "belaljenana2@gmail.com",
-        "note": "This page is for my Templates assignment",
-        "dob": date(2004, 5, 13),
-    }
-    return render(request, "pages/index.html", context)
+    people = Person.objects.all()
+    return render(request, 'pages/index.html', {'people': people})
